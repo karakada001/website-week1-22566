413192ef0104bdd9c60c8598610db76b42c01533
Khunaiko <pasuaiko21089@gmail.com>
2023-10-17 15:50:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text1">
+    <div>
+      <img src="./assets/logo.png" alt="">
+    </div>
+    <h1>Hello {{ name }} AGE: {{ age}}</h1>  
+    <p v-html="msg"></p>
+    <p v-pre>Eample: {{ name }} </p>
+  </div>
+</template>
+
+<script>
+ export default{
+    data() {
+      return {
+         name: "Mark", 
+         age: "45", 
+         msg:"<storage>Hello Vue</storage>"
+     };
+   }
+ }
+</script>
+
+<style>
+ h1{
+  color: blueviolet;
+ }
+ .text1{
+    font-size: 30px;
+ }
+</style>
